8a39970dc531247abf9587546ef9ba071fa12c69
JesusMarcano <jesus.marcano@rocketmail.com>
2015-08-15 00:12:41 -0400

    feat: Add buttons to the second scene and an VC

---
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -0,0 +1,37 @@
+//
+//  PlaySoundsViewController.swift
+//  PitchPerfect
+//
+//  Created by Jesus Marcano on 8/14/15.
+//  Copyright © 2015 Agencia Secreta 86. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class PlaySoundsViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func playSoundSlowly(sender: UIButton) {
+    }
+    
+    @IBAction func playSoundsFast(sender: UIButton) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
